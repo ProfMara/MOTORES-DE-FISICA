857b392abf636f2ca1bc132be069aa6be64c3e74
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-08-25 10:18:19 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,45 +13,15 @@ var mexendo;
 function setup() {
     createCanvas(400, 400);
 
-    //ETAPA 1: Configurar o motor de física
-    //criar o motor
-
-    //guardar o mundo
-
-
-
-    //ETAPA 2: Criar os corpos
-    //definir a propriedade do solo
-
-    //parado = {}
-   
-    //criar o corpo do solo
-
-    //adicionar o solo no mundo
-
-    //definir a propriedade da bola
-
-    //criar o corpo da bola
-
-    //adicionar a bola no mundo
-
+    engine = Engine.create();
+    world = engine.world;
 
 }
 
 
 function draw() {
     background("cyan");
-
-    //atualizar o motor
-
-
-    //pintar o solo de verde
-    fill("green")
-    //desenhar o retângulo no corpo do solo
-
-
-    //pintar a bola;
-    fill("");
-    //desenhar a ellipse na posição do corpo da bola
+    //atualiza o motor
+    Engine.update(engine);
 
 }
